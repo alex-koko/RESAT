7f218f485373f5c52e7706974b339848cfa5ef8e
alex-koko <86093028+alex-koko@users.noreply.github.com>
2023-09-19 23:04:13 +0900

    Update slideShow.js

--- day5/JS/slideShow.js
+++ day5/JS/slideShow.js
@@ -1,34 +1,51 @@
-const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
-const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
-let currentIdx = 0; //현재 슬라이드 index
-const slideCount = slideImg.length; // 슬라이드 개수
-const prev = document.querySelector('.prev'); //이전 버튼
-const next = document.querySelector('.next'); //다음 버튼
-const slideWidth = 300; //한개의 슬라이드 넓이
-const slideMargin = 100; //슬라이드간의 margin 값
+const slides = document.querySelector('.slides');
+const slideImg = document.querySelectorAll('.slides li');
+let currentIdx = 0;
+const slideCount = slideImg.length;
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slideWidth = 300;
+const slideMargin = 100;
+const autoSlideInterval = 2000; // 2초 간격
 
-//전체 슬라이드 컨테이너 넓이 설정
 slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
 
 function moveSlide(num) {
-    slides.style.left = -num * 400 + 'px';
+    slides.style.left = -num * (slideWidth + slideMargin) + 'px';
     currentIdx = num;
 }
 
 prev.addEventListener('click', function () {
-    /*첫 번째 슬라이드로 표시 됐을때는
-    이전 버튼 눌러도 아무런 반응 없게 하기 위해
-    currentIdx !==0일때만 moveSlide 함수 불러옴 */
-
     if (currentIdx !== 0) moveSlide(currentIdx - 1);
 });
 
 next.addEventListener('click', function () {
-    /* 마지막 슬라이드로 표시 됐을때는
-    다음 버튼 눌러도 아무런 반응 없게 하기 위해
-    currentIdx !==slideCount - 1 일때만
-    moveSlide 함수 불러옴 */
     if (currentIdx !== slideCount - 1) {
         moveSlide(currentIdx + 1);
+    } else {
+        // 끝 부분에서는 첫 번째 이미지로 순환
+        moveSlide(0);
     }
+});
+
+// 자동 슬라이드 함수
+function autoSlide() {
+    if (currentIdx !== slideCount - 1) {
+        moveSlide(currentIdx + 1);
+    } else {
+        moveSlide(0);
+    }
+}
+
+// 2초마다 자동 슬라이드 시작
+let autoSlideTimer = setInterval(autoSlide, autoSlideInterval);
+
+// 마우스 오버 시 자동 슬라이드 멈춤
+slides.addEventListener('mouseenter', function () {
+    clearInterval(autoSlideTimer);
+});
+
+// 마우스 아웃 시 자동 슬라이드 재시작
+slides.addEventListener('mouseleave', function () {
+    autoSlideTimer = setInterval(autoSlide, autoSlideInterval);
 });
